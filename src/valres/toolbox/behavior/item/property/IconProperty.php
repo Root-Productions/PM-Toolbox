6ab96bfdccba590fb46e4eab806ab9b159c09136
Valres <104681026+ValresMC@users.noreply.github.com>
2026-06-10 23:52:55 +0200

    Update IconProperty.php

--- src/valres/toolbox/behavior/item/property/IconProperty.php
+++ src/valres/toolbox/behavior/item/property/IconProperty.php
@@ -4,29 +4,25 @@ declare(strict_types=1);
 
 namespace valres\toolbox\behavior\item\property;
 
-use pocketmine\nbt\tag\Tag;
-use valres\toolbox\behavior\item\component\ComponentNbtHelper;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\StringTag;
 
 /** Defines the texture or texture set used for the item's UI icon. */
 final class IconProperty extends DataDrivenItemProperty {
-    public function __construct(private readonly string|array $textures) {
+    public function __construct(private readonly string $icon) {
     }
 
     public static function identifier(): string {
         return "minecraft:icon";
     }
 
-    public static function textures(string $default, ?string $dyed = null, ?string $iconTrim = null): array {
-        return [
-            "textures" => array_filter([
-                "default" => $default,
-                "dyed" => $dyed,
-                "icon_trim" => $iconTrim
-            ], static fn(mixed $value): bool => $value !== null)
-        ];
-    }
-
-    public function toNBT(): Tag {
-        return ComponentNbtHelper::tag($this->textures);
+    public function toNBT(): CompoundTag {
+        return CompoundTag::create()
+            ->setTag("texture", new StringTag($this->icon))
+            ->setTag(
+                "textures",
+                CompoundTag::create()
+                    ->setTag("default", new StringTag($this->icon))
+            );
     }
 }
